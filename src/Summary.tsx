165fef4a35e0ee030a64201509ea2f04fd685f6e
Nick Wong <nick.wong1@ibm.com>
2022-09-30 09:39:37 +0800

    Deploy

--- src/Summary.tsx
+++ src/Summary.tsx
@@ -1,13 +1,21 @@
 import { FC } from "react";
 import useCurrentPage from "./hooks/useCurrentPage";
+import styled from "styled-components";
+
+const StyledContainer = styled.div`
+    background: white; 
+    margin: 0 20px; 
+    border-radius: 10px; 
+    padding: 5px; 
+`
 
 const Summary: FC = () => {
 
     const currentPage = useCurrentPage()
 
-    return <div>
+    return <StyledContainer>
         <div>Current Page: {currentPage}</div>
-    </div>
+    </StyledContainer>
 }
 
 export default Summary
