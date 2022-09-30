4ddfee57f88d10e42ee0d3cf5eff7b12ef3de5d8
Nick Wong <nick.wong1@ibm.com>
2022-09-30 09:58:12 +0800

    Deploy

---
+++ src/Build.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+
+const Build: FC = () => {
+  return (
+    <button>Add current</button>
+  )
+}
+
+export default Build;
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import Build from './Build';
 
 const tjsRoot = document.createElement('div')
 const footer = document.getElementById('footer')
 footer?.parentNode?.insertBefore(tjsRoot, footer)
-
 const root = ReactDOM.createRoot(tjsRoot);
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+const buildingRoot = document.createElement('div')
+const upgradeBuilding = document.getElementsByClassName('.upgradeBuilding')[0]
+upgradeBuilding?.parentNode?.insertBefore(buildingRoot, upgradeBuilding)
+const broot = ReactDOM.createRoot(buildingRoot);
+broot.render(
+  <React.StrictMode>
+    <Build />
+  </React.StrictMode>
+)
